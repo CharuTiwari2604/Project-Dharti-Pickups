d17d1485b3ac461e7da9c87c62afd5f194576e6f
Charu Tiwari <wairaagiaarya@gmail.com>
2025-07-21 21:49:07 +0530

    Fix MongoDB URI for Render deployment

--- dhartiback/index.js
+++ dhartiback/index.js
@@ -38,7 +38,12 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Internal server error' });
 });
 
-mongoose.connect(process.env.DBURL)
+// mongoose.connect(process.env.DBURL)
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+
   .then(() => {
     console.log('Connected to MongoDB');
     const port = process.env.PORT || 7000;
