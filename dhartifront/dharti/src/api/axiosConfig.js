fac5c092d12628f7e27eceedcc7b7b1e3fd42665
Charu Tiwari <wairaagiaarya@gmail.com>
2025-07-22 09:16:47 +0530

    Rectify the axios

--- dhartifront/dharti/src/api/axiosConfig.js
+++ dhartifront/dharti/src/api/axiosConfig.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",  // ✅ Change to your backend URL
+  baseURL: "https://project-dharti-pickups.onrender.com/api",  // ✅ Change to your backend URL
   withCredentials: true,                // ✅ Important for cookie-based auth
   headers: {
     "Content-Type": "application/json"
