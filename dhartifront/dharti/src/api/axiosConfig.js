fa444bf88435a570f4da5e598ba3b21e6d7556fc
Charu Tiwari <wairaagiaarya@gmail.com>
2025-07-22 22:10:26 +0530

    Commiting the main url

--- dhartifront/dharti/src/api/axiosConfig.js
+++ dhartifront/dharti/src/api/axiosConfig.js
@@ -1,6 +1,5 @@
-// src/api/axiosConfig.js
 import axios from "axios";
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 console.log("Using API_URL:", API_URL);
 
 const axiosInstance = axios.create({
