90165e7e09a8ee66c60163756a383d2a205790af
Charu Tiwari <wairaagiaarya@gmail.com>
2025-07-22 09:30:37 +0530

    Changing Url in axiosConfig

--- dhartifront/dharti/src/api/axiosConfig.js
+++ dhartifront/dharti/src/api/axiosConfig.js
@@ -1,8 +1,10 @@
 // src/api/axiosConfig.js
 import axios from "axios";
+const API_URL = import.meta.env.VITE_API_URL;
 
 const axiosInstance = axios.create({
-  baseURL: "https://project-dharti-pickups.onrender.com/api",  // ✅ Change to your backend URL
+  // baseURL: "http://localhost:5000/api",  // ✅ Change to your backend URL
+   baseURL: API_URL,
   withCredentials: true,                // ✅ Important for cookie-based auth
   headers: {
     "Content-Type": "application/json"
